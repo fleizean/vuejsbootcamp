13215400675f492fd7119f7014202f588da4b473
Enes <nsyagz@gmail.com>
2023-08-14 22:42:03 +0300

    Counter-app

--- Week1/1-vue-instance/app.js
+++ Week1/1-vue-instance/app.js
@@ -1,16 +1,29 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             title: "Vue.js Bootcamp day one!",
             description: "Lorem ipsum dolor sit amet",
             eduflow: {
                 title: "Müfredat ve açıklamalar için tıklayınız.",
                 target: "_blank",
-                url: "https://eduflow.kablosuzkedi.com",
-                alt: "mufredat-kablosuzkedi-vue"
+                url: "https://github.com/fleizean",
+                alt: "mufredat-fleizean-vue"
             },
-            owner: "pogaca"
+            owner: "powagaca",
+            x: 0,
+            y: 0,
         };
+    },
+    methods : { // javascriptteki script içerisine function tanımlamak ile aynı işlev aşağıdaki
+        changeTitle(pTitle) { 
+            this.title = pTitle; // buradaki this ile üstteki return alanında oluşturmuş olduğum
+            // değişkenlere ulaşabildim thisin mantığını c++'den biliyosun.
+        },
+        updateCoords(event) {
+            console.log(event.x, event.y);
+            this.x = event.x;
+            this.y = event.y;
+        },
     },
 
 }).mount("#app");
